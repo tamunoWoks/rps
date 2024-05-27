589d1ef4d40279d94e46ad4dad6fb6195f297a67
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-27 17:11:11 -0600

    Create random viarable and computer choice

--- rps.py
+++ rps.py
@@ -2,13 +2,18 @@ import random
 
 user_wins = 0
 computer_wins = 0
+options = ['rock', "paper", 'scissors']
 
 while True:
     user_input = ("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == 'q':
         break
     
-    if user_input not in ['rock', "paper", 'scissors']:
+    if user_input not in options:
         continue
+
+    random_number = random.randint(0,2) #rock:0, paper:1, scissors:2
+    computer_pick = options[random_number]
+    print(f'Computer picked {computer_pick}.')
 
     
