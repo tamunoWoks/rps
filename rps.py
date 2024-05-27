67b990a62b29cadc09df2ddc50a3b4d17e28d6a1
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-27 17:22:50 -0600

    Implement game logic and update score

--- rps.py
+++ rps.py
@@ -16,4 +16,19 @@ while True:
     computer_pick = options[random_number]
     print(f'Computer picked {computer_pick}.')
 
-    
+    if user_input == 'rock' and computer_pick == 'scissors':
+        print('You won')
+        user_wins += 1
+    elif user_input == 'paper' and computer_pick == 'rock':
+        print('You won')
+        user_wins += 1
+    elif user_input == 'scissors' and computer_pick == 'paper':
+        print('You won')
+        user_wins += 1
+    elif user_input == computer_pick:
+        print('You won')
+        user_wins += 1
+        continue
+    else:
+        print('You lost!')
+        computer_wins += 1
