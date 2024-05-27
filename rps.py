8c30753a54af7473b12cb7f584c28bdb6e50635e
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-27 17:50:45 -0600

    Clean up code

--- rps.py
+++ rps.py
@@ -2,37 +2,37 @@ import random
 
 user_wins = 0
 computer_wins = 0
-options = ['rock', "paper", 'scissors']
+options = ["rock", "paper", "scissors"]
 
 while True:
-    user_input = ("Type Rock/Paper/Scissors or Q to quit: ").lower()
-    if user_input == 'q':
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
         break
-    
+
     if user_input not in options:
         continue
 
-    random_number = random.randint(0,2) #rock:0, paper:1, scissors:2
+    random_number = random.randint(0, 2)  # rock:0, paper:1, scissors:2
     computer_pick = options[random_number]
-    print(f'Computer picked {computer_pick}.')
+    print(f"Computer picked {computer_pick}.")
 
-    if user_input == 'rock' and computer_pick == 'scissors':
-        print('You won')
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won")
         user_wins += 1
-    elif user_input == 'paper' and computer_pick == 'rock':
-        print('You won')
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won")
         user_wins += 1
-    elif user_input == 'scissors' and computer_pick == 'paper':
-        print('You won')
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won")
         user_wins += 1
     elif user_input == computer_pick:
         print("It's a tie")
         user_wins += 1
         continue
     else:
-        print('You lost!')
+        print("You lost!")
         computer_wins += 1
 
-print(f'You won {user_wins} times')
-print(f'The computer won {computer_wins} times.')
-print('Goodbye!')
+print(f"You won {user_wins} times")
+print(f"The computer won {computer_wins} times.")
+print("Goodbye!")
